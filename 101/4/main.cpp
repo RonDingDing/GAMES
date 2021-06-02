a3cfb768c8ef6ab209757d42cb20c11a737e9401
RonDingDing <ronweasleyding@163.com>
2021-06-02 23:54:09 +0800

    hw4

--- 101/4/main.cpp
+++ 101/4/main.cpp
@@ -4,66 +4,86 @@
 
 std::vector<cv::Point2f> control_points;
 
-void mouse_handler(int event, int x, int y, int flags, void *userdata) 
+void mouse_handler(int event, int x, int y, int flags, void *userdata)
 {
-    if (event == cv::EVENT_LBUTTONDOWN && control_points.size() < 4) 
+    int key = (*(int *)userdata);
+
+    // if (event == cv::EVENT_LBUTTONDOWN && control_points.size() < 4)
+    if (event == cv::EVENT_LBUTTONDOWN && key != 's')
     {
         std::cout << "Left button of the mouse is clicked - position (" << x << ", "
-        << y << ")" << '\n';
+                  << y << ")" << '\n';
         control_points.emplace_back(x, y);
-    }     
+    }
 }
 
-void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window) 
+void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window)
 {
     auto &p_0 = points[0];
     auto &p_1 = points[1];
     auto &p_2 = points[2];
     auto &p_3 = points[3];
 
-    for (double t = 0.0; t <= 1.0; t += 0.001) 
+    for (double t = 0.0; t <= 1.0; t += 0.001)
     {
-        auto point = std::pow(1 - t, 3) * p_0 + 3 * t * std::pow(1 - t, 2) * p_1 +
-                 3 * std::pow(t, 2) * (1 - t) * p_2 + std::pow(t, 3) * p_3;
+        auto point = std::pow(1 - t, 3) * p_0 +
+                     3 * t * std::pow(1 - t, 2) * p_1 +
+                     3 * std::pow(t, 2) * (1 - t) * p_2 +
+                     std::pow(t, 3) * p_3;
 
         window.at<cv::Vec3b>(point.y, point.x)[2] = 255;
     }
 }
 
-cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, float t) 
+cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, float t)
 {
     // TODO: Implement de Casteljau's algorithm
-    return cv::Point2f();
-
+    std::vector<cv::Point2f> tem = std::vector<cv::Point2f>();
+    if (control_points.size() != 1)
+    {
+        for (int i = 1; i < control_points.size(); i ++)
+        {   
+            cv::Point2f t_vector = (control_points[i] - control_points[i-1]) * t;
+            cv::Point2f smaller = control_points[i-1] + t_vector;
+            tem.push_back(smaller);
+        }
+        return recursive_bezier(tem, t);        
+    }
+    return control_points[0];
 }
 
-void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window) 
+void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window)
 {
-    // TODO: Iterate through all t = 0 to t = 1 with small steps, and call de Casteljau's 
+    // TODO: Iterate through all t = 0 to t = 1 with small steps, and call de Casteljau's
     // recursive Bezier algorithm.
-
+    for (double t = 0.0; t <= 1.0; t += 0.001)
+    {
+        cv::Point2f point = recursive_bezier(control_points, t);
+        window.at<cv::Vec3b>(point.y, point.x)[1] = 255;
+    }
 }
 
-int main() 
+int main()
 {
     cv::Mat window = cv::Mat(700, 700, CV_8UC3, cv::Scalar(0));
     cv::cvtColor(window, window, cv::COLOR_BGR2RGB);
     cv::namedWindow("Bezier Curve", cv::WINDOW_AUTOSIZE);
-
-    cv::setMouseCallback("Bezier Curve", mouse_handler, nullptr);
-
     int key = -1;
-    while (key != 27) 
+    // cv::setMouseCallback("Bezier Curve", mouse_handler, nullptr);
+    cv::setMouseCallback("Bezier Curve", mouse_handler, &key);
+
+    while (key != 27)
     {
-        for (auto &point : control_points) 
+        for (auto &point : control_points)
         {
             cv::circle(window, point, 3, {255, 255, 255}, 3);
         }
 
-        if (control_points.size() == 4) 
+        // if (control_points.size() == 4)
+        if (key == 's')
         {
-            naive_bezier(control_points, window);
-            //   bezier(control_points, window);
+            // naive_bezier(control_points, window);
+            bezier(control_points, window);
 
             cv::imshow("Bezier Curve", window);
             cv::imwrite("my_bezier_curve.png", window);
@@ -76,5 +96,5 @@ int main()
         key = cv::waitKey(20);
     }
 
-return 0;
+    return 0;
 }
